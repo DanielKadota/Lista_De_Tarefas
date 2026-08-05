0aa6e7ba16d3d204a54bcdf17a7d4b8c116f7e48
DanielKadota <daniel.kadota07@gmail.com>
2026-08-05 18:45:08 -0300

    Seleção_Dos_Elementos_Html (Js)

---
+++ Js/app.js
@@ -0,0 +1,9 @@
+// ==============================
+// SELEÇÃO DOS ELEMENTOS DO HTML
+// ==============================
+
+const btnAdd = document.querySelector('#btnAdicionar');
+const inptTarefa = document.querySelector('#inputTarefa');
+const mensagemErro = document.querySelector('#mensagemErro');
+const listaTarefas = document.querySelector('#listaTarefas');
+const contadorTexto = document.querySelector('#contadorTexto');
